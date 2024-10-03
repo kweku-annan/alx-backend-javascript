a7a34f84d8f2dcff279c2e5542edfe012692c71f
kweku-annan <kwekuannan.dev@gmail.com>
2024-10-03 19:16:18 +0000

    Add solution for task 2

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from './2-arrow';
+
+const newNeighborhoodsList = new getNeighborhoodsList();
+const res = newNeighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
